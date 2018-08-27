6cb2bf70b25c66f13fed24ac0d6a008fa439f256
Mehathab Shaik <mehathab4eva.com>
2018-08-27 14:15:48 -0600

    second commit

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,7 +6,7 @@ const Intro = ()=> {
 		<div id='intro' className="animated lightSpeedIn delay-1s">
 				<h2>PortFolio</h2>
 				<h3>Full Stack Web Development on MERN Stack</h3>
-				<img src='./mernstack.PNG' alt="mernstack" id="mernStackPic"/> 
+				<img src='./mernstack.png' alt="mernstack" id="mernStackPic"/> 
 
 		</div>
 		)
