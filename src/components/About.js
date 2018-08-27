995c9e534032d3b2c8f7371cf59086b1ba829fdd
Mehathab Shaik <mehathab4eva.com>
2018-08-27 16:00:16 -0600

    about page update -1

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,10 @@ const About = ()=> {
 	return (
 		<div id='about' className="animated lightSpeedIn delay-1s">
 				<img src='./aboutImg.jpg' alt='aboutPic' className='aboutPic' /> 
-				<p>Full Stack Web Developer.
+				<img src='https://www.codewars.com/users/Mehathab4eva/badges/small' alt='codewars' />
+
+				<h2>Full Stack Web Developer.</h2>
+				<p>
 				A year of experience developing custom, interactive,
 				and responsive web applications while completing the U of U Coding Bootcamp. Currently seeking a role as a
 				Web Developer in a collaborative, cohesive, innovative environment where I can apply and extend my
